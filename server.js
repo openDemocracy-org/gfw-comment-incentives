2c0db4335a6f6ee0345b27c65230e6e0443619b3
Ali Blackwell <aliblackwell@protonmail.com>
2020-11-17 17:05:34 +0000

    Logging post requests to test webhooks

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser')
+const fs = require('fs');
 
 // Constants
 const PORT = 8080;
@@ -8,9 +10,34 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.post('/highlight-comment', (req,res) => {
+    console.log(req)
+    
+    fs.appendFile('public/logs.txt', req.body, (err) => {
+        if (err) throw err;
+    });
+})
+
+app.get('/highlight-comment', (req,res) => {
+    
+    fs.appendFile('public/logs.txt', req.body, (err) => {
+        if (err) throw err;
+        res.send('Created logs');
+    });
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+app.use(express.static('public'))
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
