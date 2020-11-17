6373ab1b28bbb2a6f0b299badb7d9254bfae3097
Ali Blackwell <aliblackwell@protonmail.com>
2020-11-17 17:16:24 +0000

    Stringify payload

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.post('/highlight-comment', (req,res) => {
-    console.log(req)
+    console.log(JSON.stringify(req.body))
     
-    fs.appendFile('public/logs.txt', req.body, (err) => {
+    fs.appendFile('public/logs.txt', JSON.stringify(req.body), (err) => {
         if (err) throw err;
     });
 })
