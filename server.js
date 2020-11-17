1c49801539687b5c206d042c5cec68234682f0ce
Ali Blackwell <aliblackwell@protonmail.com>
2020-11-17 17:28:51 +0000

    Check CoralTalk signature

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const fs = require('fs');
-
+const SIGNING_SECRET = "empsec_fa0e94fca0c13666c96f01c09b3c3b4673e1bd37289d848411519f3c5f46eb81f610d0";
+const crypto = require("crypto");
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -17,24 +18,74 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.post('/highlight-comment', (req,res) => {
-    console.log(JSON.stringify(req.body))
-    
-    fs.appendFile('public/logs.txt', JSON.stringify(req.body), (err) => {
+function extractBody(body, sig) {
+    // Step 1: Extract signatures from the header.
+    const signatures = sig
+        // Split the header by `,` to get a list of elements.
+        .split(",")
+        // Split each element by `=` to get a prefix and value pair.
+        .map(element => element.split("="))
+        // Grab all the elements with the prefix of `sha256`.
+        .filter(([prefix]) => prefix === "sha256")
+        // Grab the value from the prefix and value pair.
+        .map(([, value]) => value);
+
+    // Step 2: Prepare the `signed_payload`.
+    const signed_payload = body;
+
+    // Step 3: Calculate the expected signature.
+    const expected = crypto
+        .createHmac("sha256", SIGNING_SECRET)
+        .update(signed_payload)
+        .digest()
+        .toString("hex");
+
+    // Step 4: Compare signatures.
+    if (
+        // For each of the signatures on the request...
+        !signatures.some(signature =>
+            // Compare the expected signature to the signature on in the header. If at
+            // least one of the match, we should continue to process the event.
+            crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(expected))
+        )
+    ) {
+        throw new Error("Invalid signature");
+    }
+
+    // Parse the JSON for the event.
+    return JSON.parse(body.toString());
+}
+
+app.post("/highlight-comment", bodyParser.raw({ type: "application/json" }), (req, res) => {
+    const sig = req.headers["x-coral-signature"];
+
+    let body;
+
+    try {
+        // Parse the JSON for the event.
+        body = extractBody(req.body, sig);
+    } catch (err) {
+        return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+
+    fs.appendFile('public/events.txt', JSON.stringify(body), (err) => {
         if (err) throw err;
     });
-})
 
-app.get('/highlight-comment', (req,res) => {
-    
-    fs.appendFile('public/logs.txt', req.body, (err) => {
+    // Return a response to acknowledge receipt of the event
+    res.json({ received: true });
+});
+
+app.get('/highlight-comment', (req, res) => {
+
+    fs.appendFile('public/logs.txt', JSON.stringify(req.body), (err) => {
         if (err) throw err;
         res.send('Created logs');
     });
 })
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+    res.send('Hello World');
 });
 
 app.use(express.static('public'))
