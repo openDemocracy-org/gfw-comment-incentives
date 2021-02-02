d6839d481412a45e226c867a9ae9086c34358357
Ali Blackwell <aliblackwell@protonmail.com>
2021-02-02 11:49:00 +0000

    Make docs clearer with proper default .envs

--- server-fake-site.js
+++ server-fake-site.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 // Constants
-const SITE_PORT = process.env.SITE_PORT || 8080;
+const SITE_PORT = process.env.SITE_PORT || 2000;
 const SITE_HOST = process.env.SITE_HOST || '0.0.0.0';
 
 // App
